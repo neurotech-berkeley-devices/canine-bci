49e599a153007570cc59a7e7b6ff8cd615da234e
Dhruv Vaish <dvaish@berkeley.edu>
2023-03-03 16:41:19 -0800

    add FIFO functionality

--- startup/example.ino
+++ startup/example.ino
@@ -1,6 +1,5 @@
 #include <Wire.h>
-// Arduino I2C for Interfacing with ADP PPG Chip 
-// Anshul, Dhruv, Ashwin
+
 
 void write(byte reg, int data, int ADDR = 0x64) {
   Wire.beginTransmission(ADDR);   // Send SLAVE ADDR + WRITE bit
@@ -15,57 +14,28 @@ int read(byte reg, int bytes = 2, int ADDR = 0x64) {
   Wire.write(byte(reg));          // Send requested REGISTER ADDR
   Wire.endTransmission(ADDR);     // End transmission
 
-  Wire.requestFrom(ADDR, bytes, true); // Request bytes from I2C
+  Wire.requestFrom(ADDR, bytes, true); // Request bytes from I2C - SR + SLAVE ADDR + READ
 
   int index = 0;
   int data = 0;
-  while (Wire.available()) {
+  while (Wire.available()) {      // Will run twice and pull two bytes
     byte result = Wire.read();
-    data = (data << (8 * index)) + result; // Write data to from I2C to value
+    data = (data << (8 * index)) + result;  // Write data to from I2C to value
     index += 1;
   }
   
   return data;
 }
 
-int awaken() {
-  // Set to NORMAL mode
-  write(0x10, 0x2); 
-}
-
-int kill() {
-  // Set to WRITE mode
-  write(0x01, 0x1);
-
-  // Clear all interrupts
-  write(0x00, 0xFF);
-
-  int DATA_ACCESS_CTL = read(0x5F);
-  DATA_ACCESS_CTL = DATA_ACCESS_CTL | 1;
-  write(0x5F, DATA_ACCESS_CTL);
-
-  write(0x00, 0x80FF);
-
-  DATA_ACCESS_CTL = read(0x5F);
-  DATA_ACCESS_CTL = DATA_ACCESS_CTL & 0xFFFE;
-  write(0x5F, DATA_ACCESS_CTL);
-
-  // Set to STANDBY mode
-  write(0x10, 0x0);
-}
 
 int startup() {
     Wire.begin();     
-    write(0x10, 0x1); // Write 0x10 <= 0x1 to set WRITE mode
+    write(0x10, 0x1);   // Write 0x10 <= 0x1 to set WRITE mode
 
-    int clk_config = read(0x4B, 2);     // Read CLK CONFIG register
-    clk_config = clk_config | (1 << 7); // Flip the 7th bit
-    write(0x4B, clk_config);            // Write back new value to register
+    int clk_config = read(0x4B);          // Read CLK CONFIG register
+    clk_config = clk_config | (1 << 7);   // Set the 7th bit to one
+    write(0x4B, clk_config);              // Write back new value to register
 
-    return !((read(0x10) == 1) && (read(0x4B, 2) == 0x2692));
-}
-
-int example() {
     // Setting slot configurations
     write(0x30, 0x0319);  // SLOTA_LEDMODE
     write(0x35, 0x0319);  // SLOTB_LEDMODE
@@ -97,11 +67,9 @@ int example() {
     write(0x14, 0x0116);  // PD_LED_SELECT
     write(0x15, 0x0330);  // NUM_AVG - averaging factor can increase to reduce speed of FIFO writing
     write(0x4E, 0x0040);  // ADC_TIMING
-    
-    return 0;
-}
 
-int fifo(int threshold) {
+    //==============================================
+    
     // Setting FIFO interrupt threshold value
     int registerThreshold = read(0x06);
     write(0x06, registerThreshold & (0xC1FF));
@@ -113,8 +81,20 @@ int fifo(int threshold) {
     // INT_IO_CTL
     write(0x02, 0x0006);
 
-    return 0;
+    // Write 0x10 <= 0x2 to set NORMAL OPERATION mode
+    write(0x10, 0x2);
+
+    return !((read(0x10) == 2) && (read(0x4B) == 0x2692));    // 0 return means success
 }
+
+
+void standby(){
+  write(0x10, 0x0001);
+  write(0x00, 0x00FF);
+  
+  write(0x5F, (0xFFFF << 1));
+}
+
 
 void setup()
 {
@@ -122,16 +102,12 @@ void setup()
     while (!Serial);    // Waiting for Serial Monitor
     Serial.println("\nI2C Scanner");
 
-    int fail = startup();
-    if (!fail) {
-      Serial.println("SUCCESS");
-    } else {
-      Serial.println("FAIL");
-    }
+    int success = startup();
+    // Serial.println(success);
 
-    example();
-    fifo();
+    write(0x10, 0x0000);
 }
+
 
 void readFIFO()
 {
@@ -151,11 +127,12 @@ void readFIFO()
     write(0x5F, 0x0);
 }
 
+
 void loop()
 {
-  int data = digitalRead(2);
+  /*int data = digitalRead(2);
   if(data){
     readFIFO();
   }
-  delay(500);
+  delay(500);*/
 }
